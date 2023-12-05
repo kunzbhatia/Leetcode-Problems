397d4ec3045366eb78b7caf8625a6f38d40e192c
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2023-12-05 21:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (49.20%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        return n - 1;
+    }
+}
+
+// class Solution {
+//     public int numberOfMatches(int n) {
+//         int count = 0;
+//         int rev = 0;
+//         while(n>1) 
+//         {
+//             rev = n/2;
+//             count+=rev;
+//             n=n-rev;
+//         }
+//         return count;
+//     }
+// }
+
+// OR Simple logic based:
+// return n-1
