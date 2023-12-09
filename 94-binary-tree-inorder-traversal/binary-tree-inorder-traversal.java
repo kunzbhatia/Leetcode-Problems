19bbea38ae75c301ab2d5d09fa0555663fb3e984
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2023-12-09 23:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (48.33%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,53 @@
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,null,2,3]
+// Output: [1,3,2]
+// Example 2:
+
+// Input: root = []
+// Output: []
+// Example 3:
+
+// Input: root = [1]
+// Output: [1]
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+        }
+    }
+}
+
+// Time Complexity: O(n), where n is the number of nodes in the binary tree. We visit each node once.
+// Space Complexity: O(h), where h is the height of the binary tree. The space is used for the recursive call stack, and in the worst case (skewed tree), it's O(n). In the average case (balanced tree), it's O(log n).
