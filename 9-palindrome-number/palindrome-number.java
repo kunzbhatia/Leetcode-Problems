9402ae1a677bfe9c9cbf8c123cbc19eb753d1919
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-02-26 02:09:50 +0530

    Time: 4 ms (100.00%) | Memory: 43.5 MB (97.51%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        int rev=0;
+        int temp=x;
+        if(x<0)
+        {
+            return false;
+        }
+        while(x!=0)
+        {
+            int d=x%10;
+            rev=rev*10+d;
+            x=x/10;
+        }
+        if(temp==rev)
+        return true;
+        else
+        return false;
+        
+    }
+}
