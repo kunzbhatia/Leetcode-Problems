5f0ea5fc28f3f5351f0614f9940b3927a6c228b8
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-02-27 02:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (41.51%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,21 @@
+
+
+public class Solution {
+    public static int fib(int n) {
+        int[] fibo = new int[n+1];
+        fibo[0] = 0;
+        if(n==1)
+        {
+            fibo[1]=1;
+            return fibo[n];
+        }
+        if (n > 1) 
+        {
+            fibo[1] = 1;
+            for (int i = 2; i < n+1; i++) {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
+            }
+        }
+        return fibo[n];
+    }
+}
