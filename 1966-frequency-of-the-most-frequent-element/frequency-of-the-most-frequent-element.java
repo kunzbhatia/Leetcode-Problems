ad9501c49065faa672aa59f1a022f22e2daa0e80
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-02-27 02:37:39 +0530

    Time: 29 ms (90.73%) | Memory: 56.9 MB (35.42%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,47 +1,21 @@
-
-class Solution
- {
+class Solution {
     public int maxFrequency(int[] nums, int k) {
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        
-        for (int num : nums) 
-        {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-        }
-        
-        List<Integer> numsList = new ArrayList<>(freqMap.keySet());
-        Collections.sort(numsList);
-        
-        int maxCount = 0;
-        int n = numsList.size();
-        
-        for (int i = n-1; i >= 0; i--) 
-        {
-            int count = freqMap.get(numsList.get(i));   // start with frequency of nums.get(i), as we don't have to do any operation
-            int curFreq = k;
-            int j = i - 1;
-            
-            while (j >= 0 && curFreq >= (numsList.get(i) - numsList.get(j))) 
-            {
-                int diff = numsList.get(i) - numsList.get(j);
-                int diffFreq = curFreq / diff;
-                
-                if (diffFreq > freqMap.get(numsList.get(j))) 
-                {
-                    count += freqMap.get(numsList.get(j));
-                    curFreq -= freqMap.get(numsList.get(j)) * diff;
-                } 
-                else
-                 {
-                    count += diffFreq;
-                    curFreq -= diffFreq * diff;
-                }
-                j--;
+        Arrays.sort(nums);
+        int maxFreq = 0;
+        int left = 0;
+        long windowSum = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            windowSum += nums[right];
+
+            while ((long) nums[right] * (right - left + 1) - windowSum > k) {
+                windowSum -= nums[left];
+                left++;
             }
-            
-            maxCount = Math.max(maxCount, count);
+
+            maxFreq = Math.max(maxFreq, right - left + 1);
         }
-        
-        return maxCount;
+
+        return maxFreq;
     }
- }
+}
