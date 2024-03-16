9993989ef8d490336cddd68258e338e9882a59d5
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-03-16 19:55:40 +0530

    Time: 24 ms (57.31%) | Memory: 55 MB (55.25%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,11 +1,13 @@
-class Solution {
+class Solution 
+{
+    //TC O(n), SC O(n)
     public int findMaxLength(int[] nums) 
     {
         int maxLen=0;
         int n=nums.length;
         int prefixSum=0;
         Map<Integer,Integer> mpp=new HashMap<>();
-        mpp.put(0,-1);
+        mpp.put(0,-1); //for initial marking that prefix sum is zero, prefix-->index
 
         for(int i=0;i<n;i++)
         {
@@ -18,11 +20,11 @@ class Solution {
 
             if(mpp.containsKey(prefixSum))
             {
-                int currentLen=i-mpp.get(prefixSum);
-                maxLen=Math.max(maxLen,currentLen);
+                int currentLen=i-mpp.get(prefixSum); //3-(-1)= 4  or 
+                maxLen=Math.max(maxLen,currentLen);  
             }
             else
-            mpp.put(prefixSum,i);
+            mpp.put(prefixSum,i); //every sum first instance storing
         }
         return maxLen;
     }
