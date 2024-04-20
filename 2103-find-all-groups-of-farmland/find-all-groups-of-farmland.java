930982301ecffcd1f6ab48dcd5da859e5912a976
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-04-20 19:36:31 +0530

    Time: 5 ms (93.16%) | Memory: 60 MB (78.71%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.java
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.java
@@ -1,55 +1,30 @@
 class Solution {
-    int endRow = 0, endCol = 0;         //to store the end of present farm, 
-                                        // initially assumed that there are no farms hence ends at 0, 0
-    int row, col;
+    int h = 0;
+    int k = 0;
     public int[][] findFarmland(int[][] land) {
-        row = land.length;
-        col = land[0].length;
-        ArrayList<int[]> list = new ArrayList<>();
+        int m = land.length;
+        int n = land[0].length;
+        List<int[]> ans = new ArrayList<>();
         
-        for(int i = 0; i<row; i++){
-            for(int j = 0; j<col; j++){
-                if(land[i][j] == 1){
-                    // we have found the start of a farm land
-                    // r1 = i, c1 = j
-                    //we now need to find the ends, i.e, endRow and endCol for the farm
-                    //for this we can excecute a dfs to adjust out endRow and endCol 
-                    
-                    dfs(land, i, j);            //this should populate our endRow and endCol
-                    //after excecuting the dfs we must have r1 = i, c1 = j, r2 = endRow and c2 = endCol
-                    list.add(new int[]{i, j, endRow, endCol});
-                    
-                    //we need to reinitialize endRow and endCol for any nextPossible entry
-                    endRow = 0;
-                    endCol = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(land[i][j] == 1) {
+                    int u = i, v = j;
+                    dfs(land, u, v, m, n);
+                    ans.add(new int[]{i, j, h, k});
+                    h = 0; k = 0;
                 }
             }
         }
-        
-        // we have a list of entries, we just need to add into an array and return
-        // Integer[][] arr = new Integer[list.size()];       // we use Integer as toArray func returns an Object which needs to be type casted
-        // arr = list.toArray();
-        // return arr;
-        // 
-        int arr[][] = new int[list.size()][4];
-        for(int i = 0; i<arr.length; i++)arr[i] = list.get(i);
-        return arr;
-        
-        //we can also use int[] arr = list.stream().mapToInt(i -> i).toArray(); to convert 
+        return ans.toArray(new int[ans.size()][]);
     }
-    
-    public void dfs(int[][] land, int i, int j){
-        //base case - when we go beyong the boundaries OR if we reach a forest area
-        if(i<0 || j<0 || i>=row || j>=col || land[i][j] == 0)return; 
-        
-        land[i][j] = 0;
-        //marking the land as visited to avoid stackoverflow
-        dfs(land, i+1, j);      
-        //dfs(land, i-1, j);   --- I realised that if we only move right and bottom the runtime increases significantly, although I didnt understand why. Pls let me know if you understand
-        dfs(land, i, j+1);
-        //dfs(land, i, j-1);
-        
-        endRow = Math.max(endRow, i);
-        endCol = Math.max(endCol, j);
+    public void dfs(int[][] land, int u, int v, int m, int n) {
+        if(u < 0 || v < 0 || u >= m || v >= n || land[u][v] == 0) return;
+        land[u][v] = 0;
+        h = Math.max(h, u);
+        k = Math.max(k, v);
+
+        dfs(land, u+1, v, m, n);
+        dfs(land, u, v+1, m, n);
     }
 }
