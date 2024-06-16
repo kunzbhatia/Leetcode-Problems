98145a390f917285c62f838070185ce321323a38
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-06-17 02:37:03 +0530

    Time: 2 ms (15.39%) | Memory: 45.6 MB (45.52%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -25,41 +25,35 @@ class Solution
         int days=prices.length;
         int profit=0;
         // int[][]dp=new int[days+1][2];
-        int[] ahead=new int[2];
-        int[] curr=new int[2];
 
-        Arrays.fill(ahead,0);
-        Arrays.fill(curr,0);
+        // int[] ahead=new int[2];
+        // int[] curr=new int[2];
+
+        int aheadBuy,aheadNotBuy,currBuy,currNotBuy;
+        aheadBuy=aheadNotBuy=0;
+
+        // Arrays.fill(ahead,0);
+        // Arrays.fill(curr,0);
 
         // for(int[] row:dp)
         // {
         //     Arrays.fill(row,0);
         // }
 
-        ahead[0]=0;
-        ahead[1]=0;
+        // ahead[0]=0;
+        // ahead[1]=0;
 
         for(int i=days-1;i>=0;i--)
         {
-            for(int j=0;j<=1;j++)
-            {
-                if(j==1)
-                {
-                    profit=Math.max(-prices[i]+ahead[0],0+ahead[1]);
-                }
+            currBuy=Math.max(-prices[i]+aheadNotBuy,0+aheadBuy);
 
+            currNotBuy=Math.max(prices[i]+aheadBuy,0+aheadNotBuy);
 
-                else
-                {
-                    profit=Math.max(prices[i]+ahead[1],0+ahead[0]);
-                }
-
-                curr[j]=profit;
-            }
-            ahead=curr;
+            aheadBuy=currBuy;
+            aheadNotBuy=currNotBuy;
         }
 
-        return ahead[1];
+        return aheadBuy;
         
     }
 }
