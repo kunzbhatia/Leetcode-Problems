ff88f1a29aaa0b9b432a88b0682ac859e66ef603
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-06-17 04:29:02 +0530

    Time: 102 ms (19.15%) | Memory: 76.9 MB (17.97%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -25,14 +25,32 @@ class Solution
     {
         int n=nums.length;
 
-        int[][]dp=new int[n][n+1];
+        // int[][]dp=new int[n][n+1];
+        int[][]dp=new int[n+1][n+1];
 
         for(int [] row:dp)
         {
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
 
-        return lengthOfLISRecur(0,-1,nums,dp);
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i-1;j>=-1;j--)
+            {
+                int nottake=0+dp[i+1][j+1];
+                //TAKE
+                int take=Integer.MIN_VALUE;
+                if(j==-1 || nums[i]>nums[j])
+                {
+                    take=1+dp[i+1][i+1];
+                }
+
+                dp[i][j+1]=Math.max(take,nottake);
+
+            }
+        }
+
+        return dp[0][-1+1];
 
     }
 }
