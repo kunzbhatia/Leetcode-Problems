534b91216a322537d92648f46f64d9901b527e0d
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-06-17 00:54:31 +0530

    Time: 12 ms (86.03%) | Memory: 48.8 MB (52.66%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,38 @@
+class Solution 
+{
+    public int numDistinctRecur(int i,int j,char[] str1, char[] str2,int[][]dp) 
+    {
+        if(j<0) return 1; //matching done as t exhaust
+        if(i<0) return 0; //s string exhaust not found
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(str1[i]==str2[j])
+        {
+            return dp[i][j]=numDistinctRecur(i-1,j-1,str1,str2,dp)+numDistinctRecur(i-1,j,str1,str2,dp);
+        }
+        else
+        {
+            return dp[i][j]=numDistinctRecur(i-1,j,str1,str2,dp);
+        }
+    }
+
+    public int numDistinct(String s, String t) 
+    {
+        char[] str1=s.toCharArray();
+        char[] str2=t.toCharArray();
+
+        int n=s.length();
+        int m=t.length();
+
+        int[][]dp=new int[n][m];
+
+        for(int[] row: dp)
+        {
+            Arrays.fill(row,-1);
+        }
+
+        return numDistinctRecur(n-1,m-1,str1,str2,dp);
+
+    }
+}
