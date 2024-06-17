183fb9f388f3c3d8d9baecd8c5ac738b6e5d6bfa
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-06-17 11:34:52 +0530

    Time: 72 ms (64.71%) | Memory: 42.2 MB (80.60%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -5,6 +5,7 @@ class Solution
         if(i>j) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
+
         int maxi=-1000000000;
         for(int k=i;k<=j;k++)
         {
@@ -24,12 +25,27 @@ class Solution
         newnums.add(1);
 
         int n=nums.length;
-        int[][] dp=new int[n+1][n+1];
+        int[][] dp=new int[n+2][n+2];
         for(int []row:dp)
         {
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
 
-        return maxCoinsRecur(1,n,newnums,dp);
+        for(int i=n;i>0;i--)
+        {
+            for(int j=i;j<=n;j++)
+            {
+                if(i>j) continue;
+                int maxi=-1000000000;
+                for(int k=i;k<=j;k++)
+                {
+                    int cost=newnums.get(i-1)*newnums.get(k)*newnums.get(j+1)+dp[i][k-1]+ dp[k+1][j];
+                    maxi=Math.max(maxi,cost);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+
+        return dp[1][n];
     }
 }
