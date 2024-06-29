fc2489b509fe4fe12044d54ddf837761d3cbe104
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-06-29 20:40:31 +0530

    Time: 775 ms (90.95%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,15 +1,38 @@
 # Write your MySQL query statement below
 
-Select D.name as department,E1.name as employee,E1.salary as salary
+#Approach one
+-- Select d.name as department,e1.name as employee,e1.salary as salary
 
-from Employee E1 join Department d
+-- from Employee e1 join Department d
 
-on e1.DepartmentId=d.Id
+-- on e1.DepartmentId=d.Id
 
-where 3> 
-(
-    select count(distinct e2.salary)
-    from Employee E2
-    where e2.salary>e1.salary and e1.DepartmentId=e2.DepartmentId
-)
+-- where 3> 
+-- (
+--     select count(distinct e2.salary)
+--     from Employee E2
+--     where e2.salary>e1.salary and e1.DepartmentId=e2.DepartmentId
+-- )
+
+
+#Approach two
+SELECT 
+    temp.department,
+    temp.employee,
+    temp.salary
+FROM (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS department_rank
+    FROM 
+    employee e
+    LEFT JOIN 
+    department d 
+    ON e.departmentId = d.id
+) AS temp
+
+WHERE 
+    temp.department_rank <= 3;
 
