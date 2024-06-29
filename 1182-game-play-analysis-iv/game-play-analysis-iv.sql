6f8a3a4a728a20995a713cbe16e33c92c6ef43e8
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-06-29 18:30:16 +0530

    Time: 692 ms (68.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+
+Select
+round(sum(player_login)/count(distinct player_id),2) as fraction
+from
+(
+Select player_id,
+datediff(event_date, min(event_date) over(partition by player_id))=1 as player_login
+from activity
+)
+as new_table;
