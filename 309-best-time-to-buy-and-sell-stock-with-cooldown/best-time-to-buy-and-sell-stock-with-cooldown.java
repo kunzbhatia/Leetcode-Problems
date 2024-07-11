ffeec131e1865268fab3d22ddd152fdbae7f4395
Kunal Bhatia <117523747+kunzbhatia@users.noreply.github.com>
2024-07-11 23:08:52 +0530

    Time: 1 ms (82.12%) | Memory: 41.5 MB (71.03%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,40 +2,37 @@ class Solution {
      public int maxProfit(int[] prices) 
     {
         int days=prices.length;
-        // int[][]dp=new int[days+2][2];
-        int[] after2=new int[2];
-        int[] after1=new int[2];
-        int[] curr=new int[2];
+        int profit=0;
+        int[][]dp=new int[days+2][2];
 
-        Arrays.fill(after2,0);
-        Arrays.fill(after1,0);
-        Arrays.fill(curr,0);
+        for(int[] row:dp)
+        {
+            Arrays.fill(row,0);
+        }
 
-        // for(int[] row:dp)
-        // {
-        //     Arrays.fill(row,0);
-        // }
-
-        // dp[days][0]=0;
-        // dp[days][1]=0;
+        dp[days][0]=0;
+        dp[days][1]=0;
 
         for(int i=days-1;i>=0;i--)
         {
-  
-        curr[1]=Math.max(-prices[i]+after1[0],0 + after1[1]);
-        
-        curr[0]=Math.max(prices[i]+after2[1],0 + after1[0]);  
+            for(int j=0;j<=1;j++)
+            {
+                if(j==1)
+                {
+                    profit=Math.max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                }
 
-        //after2=after1;
-        // System.arraycopy(after1,0,after2,0,2); OR
-        after2=after1.clone();
-        //after1=curr;
-        // System.arraycopy(curr,0,after1,0,2); OR
-        after1=curr.clone();
 
+                else
+                {
+                profit=Math.max(prices[i]+dp[i+2][1],0+dp[i+1][0]);
+                }
+
+                dp[i][j]=profit;
+            }
         }
 
-        return curr[1];
+        return dp[0][1];
         
     }
 }
